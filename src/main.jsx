a092484bc847c8927a58280f4680fef9848c70b7
Mintu Sikder <mintusikder15@gmail.com>
2024-08-30 11:49:14 +0600

    added routes and layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,37 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
 import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './layout/Main';
+import Home from './components/Home';
+import Login from './components/Login';
+import Registration from './components/Registration';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {path:"/",
+        element:<Home></Home>
+      },
+      {path:"/login",
+        element:<Login></Login>
+      },
+      {path:"/registration",
+        element:<Registration></Registration>
+      },
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+ <RouterProvider router={router} />
   </StrictMode>,
 )
